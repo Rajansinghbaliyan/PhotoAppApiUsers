416bfe148d76d93c9be37654869810c4a9ea3604
Rajan <53229081+Rajansinghbaliyan@users.noreply.github.com>
2022-04-20 15:35:22 +0530

    added the logger

--- src/main/kotlin/io/cherrytechnologies/photoappapiusers/web/controllers/UsersController.kt
+++ src/main/kotlin/io/cherrytechnologies/photoappapiusers/web/controllers/UsersController.kt
@@ -1,14 +1,18 @@
 package io.cherrytechnologies.photoappapiusers.web.controllers
 
+import io.cherrytechnologies.photoappapiusers.utils.logInfo
 import io.cherrytechnologies.photoappapiusers.utils.responseOk
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.logging.Logger
 
 @RestController
 @RequestMapping("/v1/api/users")
 class UsersController {
 
+    val log: Logger = Logger.getLogger(UsersController::class.toString())
+
     @GetMapping("/hello")
-    fun greetings() = "Hello is working".responseOk()
+    fun greetings() = "Hello is working".responseOk().logInfo(log," GET: users")
 }

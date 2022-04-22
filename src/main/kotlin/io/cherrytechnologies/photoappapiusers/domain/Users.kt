a7afb00594b5e73c8fb7543969ca522b69f95bb8
Rajan <53229081+Rajansinghbaliyan@users.noreply.github.com>
2022-04-22 11:42:24 +0530

    fix: unique constraint on email

--- src/main/kotlin/io/cherrytechnologies/photoappapiusers/domain/Users.kt
+++ src/main/kotlin/io/cherrytechnologies/photoappapiusers/domain/Users.kt
@@ -3,7 +3,9 @@ package io.cherrytechnologies.photoappapiusers.domain
 import io.cherrytechnologies.photoappapiusers.web.dto.UserDto
 import java.sql.Timestamp
 import java.util.*
+import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.UniqueConstraint
 
 @Entity
 data class Users(
@@ -13,6 +15,7 @@ data class Users(
     override var version: Long? = null,
     var firstName: String?,
     var lastName: String?,
+    @Column(unique = true)
     var email: String?,
     var password: String?,
 ) : Base(id, version, createdDate, lastModifiedDate) {
